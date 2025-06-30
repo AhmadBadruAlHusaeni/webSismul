348387916057096e3cb301c0c7ff122d8efe84f5
AhmadBadruAlHusaeni <badrualhusaeni23@gmail.com>
2025-06-30 19:27:00 +0700

    Fix request handling crash

--- app.py
+++ app.py
@@ -1,21 +1,26 @@
-from flask import Flask, render_template, request, redirect, send_file
+from flask import Flask, render_template, request, send_file
 import os
 from audio_utils import embed_message_in_audio, extract_message_from_audio
 from image_utils import embed_message_pvd, extract_message_pvd
 from video_utils import embed_message_in_video, extract_message_from_video
 
 app = Flask(__name__)
+
 UPLOAD_FOLDER = 'static'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+
+# --- HOME ---
 @app.route('/')
 def home():
     return render_template('home.html')
 
+
 # --- AUDIO ---
 @app.route('/audio', methods=['GET', 'POST'])
 def audio():
+    extracted = ""
     if request.method == 'POST':
         if 'embed' in request.form:
             audio_file = request.files['cover_audio']
@@ -25,17 +30,20 @@ def audio():
             output = filename.replace('.wav', '_stego.wav')
             embed_message_in_audio(filename, message, output)
             return send_file(output, as_attachment=True)
+
         elif 'extract' in request.form:
             stego_file = request.files['stego_audio']
             filename = os.path.join(app.config['UPLOAD_FOLDER'], stego_file.filename)
             stego_file.save(filename)
-            message = extract_message_from_audio(filename)
-            return render_template('audio.html', extracted=message)
-    return render_template('audio.html')
+            extracted = extract_message_from_audio(filename)
+
+    return render_template('audio.html', extracted=extracted)
+
 
 # --- IMAGE ---
 @app.route('/image', methods=['GET', 'POST'])
 def image():
+    extracted = ""
     if request.method == 'POST':
         if 'embed' in request.form:
             image_file = request.files['cover_image']
@@ -45,17 +53,20 @@ def image():
             output = filename.replace('.png', '_stego.png')
             embed_message_pvd(filename, message, output)
             return send_file(output, as_attachment=True)
+
         elif 'extract' in request.form:
             stego_file = request.files['stego_image']
             filename = os.path.join(app.config['UPLOAD_FOLDER'], stego_file.filename)
             stego_file.save(filename)
-            message = extract_message_pvd(filename)
-            return render_template('image.html', extracted=message)
-    return render_template('image.html')
+            extracted = extract_message_pvd(filename)
+
+    return render_template('image.html', extracted=extracted)
+
 
 # --- VIDEO ---
 @app.route('/video', methods=['GET', 'POST'])
 def video():
+    extracted = ""
     if request.method == 'POST':
         if 'embed' in request.form:
             video_file = request.files['cover_video']
@@ -65,15 +76,17 @@ def video():
             output = filename.replace('.avi', '_stego.avi')
             embed_message_in_video(filename, message, output)
             return send_file(output, as_attachment=True)
+
         elif 'extract' in request.form:
             stego_file = request.files['stego_video']
             filename = os.path.join(app.config['UPLOAD_FOLDER'], stego_file.filename)
             stego_file.save(filename)
-            message = extract_message_from_video(filename)
-            return render_template('video.html', extracted=message)
-    return render_template('video.html')
+            extracted = extract_message_from_video(filename)
 
+    return render_template('video.html', extracted=extracted)
+
+
+# --- RUN ---
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
